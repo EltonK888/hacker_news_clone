e534024e1414f689d7bb1ca4fb57c0b04a89c604
Elton Kok <elton.kok@mail.utoronto.ca>
2019-06-04 15:51:35 -0400

    Update story links to open in new tab

--- src/components/story.jsx
+++ src/components/story.jsx
@@ -7,14 +7,14 @@ export const Story = (props) => {
         if (props.darkMode) {
             return (
                 <div className="dark-mode-story">
-                    <h5 className="storyTitle"><a className="titleLink" href={props.titleLink}>{props.title}</a></h5>
+                    <h5 className="storyTitle"><a className="titleLink" href={props.titleLink} target="_blank">{props.title}</a></h5>
                     <p className="storyDetails">{props.points} points by <Link to={`/user/${props.by}`} className="dark-mode-link">{props.by}</Link> on {props.time} with <Link to={`/post/${props.id}`} className="dark-mode-link">{props.numComments}</Link> comments</p>
                 </div>
             )
         } else {
             return (
                 <div className="story">
-                    <h5 className="storyTitle"><a className="titleLink" href={props.titleLink}>{props.title}</a></h5>
+                    <h5 className="storyTitle"><a className="titleLink" href={props.titleLink} target="_blank">{props.title}</a></h5>
                     <p className="storyDetails">{props.points} points by <Link to={`/user/${props.by}`} className="descriptionLink">{props.by}</Link> on {props.time} with <Link to={`/post/${props.id}`} className="descriptionLink">{props.numComments}</Link> comments</p>
                 </div>
             )
