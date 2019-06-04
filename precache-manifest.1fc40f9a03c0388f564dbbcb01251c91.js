9ea8a913855ccce3ce3ae23f4e2e7f367cb13df9
Elton Kok <elton.kok@mail.utoronto.ca>
2019-06-04 14:21:45 -0400

    Updates

---
+++ precache-manifest.1fc40f9a03c0388f564dbbcb01251c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3c45b2af6cdaf27a889d8bd21e4043",
+    "url": "/hacker_news_clone/index.html"
+  },
+  {
+    "revision": "1108c445e52c9b748e17",
+    "url": "/hacker_news_clone/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2a5366d85ae5a909488e",
+    "url": "/hacker_news_clone/static/css/main.64fc47e1.chunk.css"
+  },
+  {
+    "revision": "1108c445e52c9b748e17",
+    "url": "/hacker_news_clone/static/js/2.882b7579.chunk.js"
+  },
+  {
+    "revision": "2a5366d85ae5a909488e",
+    "url": "/hacker_news_clone/static/js/main.e4a9e5f1.chunk.js"
+  },
+  {
+    "revision": "f64333e9b6a95fd46a78",
+    "url": "/hacker_news_clone/static/js/runtime~main.b7fd9526.js"
+  }
+]);
