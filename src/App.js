faf0f042bb19f6f64c91bc39ec36858a3e0ab1a2
Elton Kok <elton.kok@mail.utoronto.ca>
2019-06-16 12:58:49 -0400

    Change App component to functional component

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react"
+import { HashRouter as Router, Route } from "react-router-dom";
+import User from "./components/user";
+import Post from "./post";
+import Header from "./components/header";
+import Navbar from "./components/navbar";
+import Stories from "./stories";
+
+const App = () => {
+
+
+    const getInitialTheme = () => {
+        const theme = JSON.parse(localStorage.getItem("dark"));
+        return theme || false;
+    }
+
+    const [darkMode, setDarkMode] = useState(() => getInitialTheme());
+
+    useEffect(() => {
+        if (darkMode) {
+            document.body.style.backgroundColor = "#1a1919";
+        } else {
+            document.body.style.backgroundColor = "#e4e4e4";
+        }
+    }, [darkMode])
+
+
+    /* Change between darkmode and light mode */
+    const handleThemeChange = () => {
+        if (darkMode) {
+            document.body.style.backgroundColor = "#e4e4e4";
+            document.body.style.transition = "0.25s ease-in-out"
+        } else {
+            document.body.style.backgroundColor = "#1a1919";
+            document.body.style.transition = "0.25s ease-in-out"
+        }
+        setDarkMode(!darkMode);
+        /* Saves dark mode between refreshes and different sessions */
+        localStorage.setItem("dark", JSON.stringify(!darkMode))
+    }
+    return (
+            <div className={darkMode ? "dark-mode" : "light-mode"}>
+                <Router basename="/">
+                <Navbar darkMode={darkMode} themeChange={() => handleThemeChange()}/>
+                <div className="container">
+                        <Header darkMode={darkMode}/>
+                        <Route path="/" exact render={(props) => <Stories {...props} darkMode={darkMode}/>}/>
+                        <Route path="/:type" exact render={(props) => <Stories {...props} darkMode={darkMode}/>}/>
+                        <Route path="/user/:id" exact render={(props) => <User {...props} darkMode={darkMode}/>}/>
+                        <Route path="/post/:id" exact render={(props) => <Post {...props} darkMode={darkMode}/>}/>
+                </div>
+                </Router>
+            </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,64 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Stories from "./stories";
 import "bootstrap/dist/css/bootstrap.css";
 import "./main.css";
-import { HashRouter as Router, Route } from "react-router-dom";
-import User from "./components/user";
-import Post from "./post";
-import Header from "./components/header";
-import Navbar from "./components/navbar";
-
-class App extends Component {
-    state = {
-        darkMode: this.getInitialTheme()
-    }
-    componentWillMount() {
-        if (this.state.darkMode) {
-            document.body.style.backgroundColor = "#1a1919";
-        } else {
-            document.body.style.backgroundColor = "#e4e4e4";
-        }
-    }
-    /* Saves dark mode between refreshes and different sessions */
-    componentDidUpdate() {
-        localStorage.setItem("dark", JSON.stringify(this.state.darkMode));
-    }
-    getInitialTheme() {
-        const theme = JSON.parse(localStorage.getItem("dark"));
-        return theme || false;
-    }
-    /* Change between darkmode and light mode */
-    handleThemeChange() {
-        if (this.state.darkMode) {
-            document.body.style.backgroundColor = "#e4e4e4";
-            document.body.style.transition = "0.25s ease-in-out"
-        } else {
-            document.body.style.backgroundColor = "#1a1919";
-            document.body.style.transition = "0.25s ease-in-out"
-        }
-        this.setState({darkMode: !this.state.darkMode});
-    }
-    render() {
-        const { darkMode } = this.state;
-        return (
-            <div className={darkMode ? "dark-mode" : "light-mode"}>
-                <Router basename="/">
-                <Navbar darkMode={darkMode} themeChange={() => this.handleThemeChange()}/>
-                <div className="container">
-                        <Header darkMode={darkMode}/>
-                        <Route path="/" exact render={(props) => <Stories {...props} darkMode={darkMode}/>}/>
-                        <Route path="/:type" exact render={(props) => <Stories {...props} darkMode={darkMode}/>}/>
-                        <Route path="/user/:id" exact render={(props) => <User {...props} darkMode={darkMode}/>}/>
-                        <Route path="/post/:id" exact render={(props) => <Post {...props} darkMode={darkMode}/>}/>
-                </div>
-                </Router>
-            </div>
-        )
-    }
-}
-
-
+import App from "./App"
 
 
 ReactDOM.render(<App/>, document.getElementById("root"));
